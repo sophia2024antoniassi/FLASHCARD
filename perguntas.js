14ae249c790c13436fa1a4d0181d38cc0c2358c0
sophia2024antoniassi <00001105910386sp@al.educacao.sp.gov.br>
2024-11-27 09:12:21 -0300

    projeto final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matematica',
+    'qaul e a raiz de 144?',
+    '12'
+)
+
+criaCartao(
+    'matematica',
+    'quanto é 5x5x6?',
+    '150'
+)
+
+criaCartao(
+    'biologia ',
+    'qual e o maior orgao do corpo humano?',
+    'pele'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se fala porfavor em Inglês?',
+    'please'
+)
